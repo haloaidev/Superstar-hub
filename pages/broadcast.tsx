e6100cc993c5f60f61e2912567f5888444be9330
haloaidev <troxalight@gmail.com>
2026-07-26 16:47:45 -0500

    Add component styles and pages for React UI

---
+++ pages/broadcast.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BroadcastUI } from '../components/BroadcastUI';
+
+const BroadcastPage: React.FC = () => {
+  return (
+    <div>
+      <BroadcastUI />
+    </div>
+  );
+};
+
+export default BroadcastPage;
